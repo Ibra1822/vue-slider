8e74320e96aa96d12bd893f63fc05d3dd365cd42
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-11-03 19:30:14 +0100

    fix elements

--- js/script.js
+++ js/script.js
@@ -4,28 +4,37 @@ createApp({
 data(){
 return{
 
-foto:[
- 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-
- 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-
- 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-
-'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-
-'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+all:[
+  {
+    titolo: 'Svezia',
+    desc: 'Temperature molto basse, Capitale: Stockholma',
+    foto: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+   },
+   {
+    titolo: 'Peru',
+    desc: 'Molto soleggiata, Capitale: Lima',
+    foto: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'
+   },
+   {
+    titolo: 'Chile',
+    desc: 'Grossa popolazione, Capitale : Santiago',
+    foto: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
+   },
+   {
+    titolo: 'Argentina',
+    desc: 'Belle persone, Capitale: Buenos Aires',
+    foto: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'
+   },
+   {
+    titolo: 'Colombia',
+    desc: 'Molto caldo, Capitale : Bogotà ',
+    foto: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+   }   
 
 ],
 
-titolo:[
-  'Svezia','Peru','Chile','Argentina','Colombia'
-],
-
-desc:[
-  'Temperature molto basse, Capitale: Stockholma','Molto soleggiata, Capitale: Lima','Grossa popolazione, Capitale : Santiago','Belle persone, Capitale: Buenos Aires','Molto caldo, Capitale : Bogotà '
-
-],
 counter : 0
+
 
 }},
 
@@ -37,14 +46,39 @@ this.counter = index;
 
 next(){
   this.counter++
-  
+  if(this.counter == this.all.length){
+    this.counter = 0
+  }
 },
 
 prev(){
   this.counter--
-}
+  if(this.counter < 0){
+    this.counter = this.all.length -1
+  }
+},
+
+autoPlay(){
+
+let clock = setInterval(()=>{
+
+this.next()
+
+},3000)
+
+},
+
+stopAutoPlay(){
 
 }
+
+},
+
+mounted(){
+this.autoPlay()
+ 
+}
+
 
 
 
